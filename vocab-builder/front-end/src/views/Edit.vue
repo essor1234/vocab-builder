f1b5f9ee725155c175bee4c796135670a3fb4045
Your Name <essor1234>
2024-07-23 22:24:49 +0700

    start editing a word

---
+++ vocab-builder/front-end/src/views/Edit.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Edit Word</h1>
+        <word-form :word=this.word></word-form>
+    </div>
+</template>
+
+<script>
+import { api } from "../helpers/helpers";
+import WordForm from "../components/WordForm.vue"
+
+export default ({
+    name: 'edit',
+    components: {
+        'word-form': WordForm
+    },
+    data: function(){
+        return {
+            word: {}
+        };
+    },
+    async mounted(){
+        this.word = await api.getWord(this.$route.params.id)
+    }
+})
+</script>
