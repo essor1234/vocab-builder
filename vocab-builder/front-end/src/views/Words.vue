c38c49598b125b3a15350420a544f5446877f004
Your Name <essor1234>
2024-07-27 12:43:11 +0700

    upgrade search function

--- vocab-builder/front-end/src/views/Words.vue
+++ vocab-builder/front-end/src/views/Words.vue
@@ -2,10 +2,9 @@
     <div>
         <h1>Words</h1>
         <!-- Searching bar -->
-        <input type="text" v-model="input" placeholder="Search words..." />
-        <p>{{input}}</p>
-        <p>{{filteredList}}</p>
-        <p>{{words}}</p>
+
+    <input type="text" v-model="input" placeholder="Search words..." />
+
 
 
         <table id="words" class="ui celled compact table">
@@ -38,7 +37,8 @@
 </template>
 <script>
     import {api} from '../helpers/helpers';
-    import { ref } from "vue";
+
+    import { Vue } from 'vue';
 
 
     export default {
@@ -61,12 +61,15 @@
             
 
         },
+        
         computed: {
              filteredList() {
                 // initialize as an empty string
             const searchTerm = this.input ? this.input.toLowerCase() : '';
             return this.words.filter(word => 
-                word.english.toLowerCase().includes(searchTerm)
+                (word.english.toLowerCase().includes(searchTerm) || 
+                word.french.toLowerCase().includes(searchTerm) || 
+                word.german.toLowerCase().includes(searchTerm))
             );
         }
         },
@@ -76,3 +79,53 @@
         }
     }
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
+
+body {
+  padding: 20px;
+  min-height: 100vh;
+  background-color: rgb(234, 242, 255);
+}
+
+input {
+  display: block;
+  width: 350px;
+  margin: 20px auto;
+  padding: 10px 45px;
+  background: white url("../assets/search-icon.svg") no-repeat 15px center;
+  background-size: 15px 15px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+}
+
+.item {
+  width: 350px;
+  margin: 0 auto 10px auto;
+  padding: 10px 20px;
+  color: white;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
+    rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+}
+
+.fruit {
+  background-color: rgb(97, 62, 252);
+  cursor: pointer;
+}
+
+.error {
+  background-color: tomato;
+}
+</style>
